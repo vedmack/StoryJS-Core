e9541a6b7b456a49ea5ba25a58aae02608a0047f
Zach Wise <zach@digitalartwork.net>
2012-08-31 17:29:34 -0500

    Added ability to use `now` as a data option
    You can specify how fine grained you want now to be by using `now hours minutes seconds milliseconds` as options for detail

--- Core/VMM.Date.js
+++ Core/VMM.Date.js
@@ -92,6 +92,29 @@ if(typeof VMM != 'undefined' && typeof VMM.Date == 'undefined') {
 					if (	date_array[2]			) {	date.setFullYear(		date_array[2]);			}
 					if (	date_array[0]	> 1		) {	date.setMonth(			date_array[0] - 1);		}
 					if (	date_array[1]	> 1		) {	date.setDate(			date_array[1]);			}
+				} else if (d.match("now")) {
+					var now = new Date();					
+					date.setFullYear(now.getFullYear());
+					date.setMonth(now.getMonth());
+					date.setDate(now.getDate());
+					if (d.match("hours")) {
+						date.setHours(now.getHours());
+					}
+					if (d.match("minutes")) {
+						date.setHours(now.getHours());
+						date.setMinutes(now.getMinutes());
+					}
+					if (d.match("seconds")) {
+						date.setHours(now.getHours());
+						date.setMinutes(now.getMinutes());
+						date.setSeconds(now.getSeconds());
+					}
+					if (d.match("milliseconds")) {
+						date.setHours(now.getHours());
+						date.setMinutes(now.getMinutes());
+						date.setSeconds(now.getSeconds());
+						date.setMilliseconds(now.getMilliseconds());
+					}
 				} else if (d.length <= 5) {
 					date.setFullYear(parseInt(d, 10));
 					date.setMonth(0);
