360f36e1157477c4e1c16b6711dcbf0efb70f5d1
Zach Wise <zach@digitalartwork.net>
2012-09-05 20:05:57 -0500

    Latest version

--- Embed/Embed.js
+++ Embed/Embed.js
@@ -54,7 +54,7 @@ function createStoryJS(c, src) {
 	================================================== */
 	var storyjs_embedjs, t, te, x,
 		isCDN					= false,
-		js_version				= "2.12",
+		js_version				= "2.14",
 		jquery_version_required	= "1.7.1",
 		jquery_version			= "",
 		ready = {
